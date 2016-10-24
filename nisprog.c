1bf72305f8a7b85b8a615dceac7fab94692bba2a
fenugrec <fenugrec@users.sourceforge.net>
2016-10-24 01:22:54 -0400

    indent; also use tmpfile() for nislib debug output

--- nisprog.c
+++ nisprog.c
@@ -49,38 +49,50 @@ int do_init(void) {
 }
 
 int main(int argc, char **argv) {
-	int i ;
-	const char *startfile=NULL;	/* optional commands to run at startup */
+		int i ;
+		const char *startfile=NULL;	/* optional commands to run at startup */
 
-	for ( i = 1 ; i < argc ; i++ ) {
-		if ( argv[i][0] == '-' || argv[i][0] == '+' ) {
-			switch ( argv[i][1] ) {
-			case 'f' :
-				i++;
-				if (i < argc) {
-					startfile = argv[i];
-				} else {
-					do_usage();
-					return 1;
-				}
-				break;
-			case 'h' : do_usage() ; return 0 ;
-			default : do_usage() ; return 1 ;
-			}
-		} else {
-			do_usage() ;
-			return 1;
+		dbg_stream = tmpfile();
+		if (!dbg_stream) {
+				printf("can't create temp file !\n");
+				goto badexit;
 		}
-	}
 
-	/* Input buffer */
+		for ( i = 1 ; i < argc ; i++ ) {
+				if ( argv[i][0] == '-' || argv[i][0] == '+' ) {
+						switch ( argv[i][1] ) {
+								case 'f' :
+										i++;
+										if (i < argc) {
+												startfile = argv[i];
+										} else {
+												do_usage();
+												goto badexit;
+										}
+										break;
+								case 'h' : do_usage() ; goto goodexit;
+								default : do_usage() ; goto badexit;
+						}
+				} else {
+						do_usage() ;
+						goto badexit;
+				}
+		}
 
-	do_init();
+		/* Input buffer */
 
-	enter_cli(NP_PROGNAME, startfile, np_cmdtable);
+		do_init();
 
-	/* Done */
-	return 0;
+		enter_cli(NP_PROGNAME, startfile, np_cmdtable);
+
+		/* Done */
+goodexit:
+		if (dbg_stream) fclose(dbg_stream);
+		return 0;
+
+badexit:
+		if (dbg_stream) fclose(dbg_stream);
+		return 1;
 }
 
 const struct cmd_tbl_entry np_cmdtable[]=
