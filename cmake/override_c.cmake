120a3ddc42c20a06795b4e108e2bb548c47bc799
fenugrec <fenugrec@users.sourceforge.net>
2019-11-15 12:20:19 -0500

    prepare 1.03, bump PKGVERSION

--- cmake/override_c.cmake
+++ cmake/override_c.cmake
@@ -4,5 +4,7 @@ if (CMAKE_COMPILER_IS_GNUCC)
 	set(CMAKE_C_FLAGS_DEBUG_INIT "-O0 -g")
 	set(CMAKE_C_FLAGS_MINSIZEREL_INIT "-Os")
 	set(CMAKE_C_FLAGS_RELEASE_INIT "-O3")
-	set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-O2 -gsplit-dwarf")
+	set(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-O2 -g")
+# proper way to split debugging info : see
+# https://sourceware.org/gdb/onlinedocs/gdb/Separate-Debug-Files.html
 endif ()
